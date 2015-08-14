7074e733c121d493dbe8197925e6f9e02f573ba3
Dave Hunt <dave.hunt@gmail.com>
2015-08-14 14:55:56 +0100

    Modal dialog has been removed

--- pages/start_page.py
+++ pages/start_page.py
@@ -2,23 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as expected
-from selenium.webdriver.support.ui import WebDriverWait
-
 from pages.base import Base
 
 
 class StartPage(Base):
 
     _page_title = 'Firefox Affiliates'
-    _close_modal_locator = (By.CSS_SELECTOR, '#modal button.close')
 
     def __init__(self, testsetup, open_url=True):
         """ Creates a new instance of the class and gets the page ready for testing """
         Base.__init__(self, testsetup)
         if open_url:
             self.open('/')
-            self.selenium.find_element(*self._close_modal_locator).click()
-            WebDriverWait(self.selenium, testsetup.timeout).until(
-                expected.invisibility_of_element_located(self._close_modal_locator))
